92b6afe74bce8eb06df5d7687c2ff0e216d94b52
cuihaiying <cuihaiying@cncic.chemchina.com>
2019-12-18 08:40:48 +0800

    init

---
+++ app/src/main/java/com/heynchy/douyin/videodownload/MainActivity.java
@@ -0,0 +1,13 @@
+package com.heynchy.douyin.videodownload;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
